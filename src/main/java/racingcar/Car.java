bb283f55c8efb515973fd715a2b61044027d6ca4
blackarea <supersute142@gmail.com>
2022-09-06 19:00:28 +0900

    Car 클래스, 테스트 클래스 작성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int currentPosition = 1;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if(name.length() > 5){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void moveForward() {
+        currentPosition++;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void tryMove() {
+        if(createRandomNumber() > 3) {
+            moveForward();
+        }
+    }
+
+    public int createRandomNumber(){
+        return (int) (Math.random() * 10);
+    }
+
+}
---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RacingCarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setup(){
+        car = new Car("kali");
+    }
+
+    @Test
+    @DisplayName("전진")
+    void carForward(){
+        car.moveForward();
+        assertThat(car.getCurrentPosition()).isEqualTo(2);
+    }
+
+    @Test
+    void randomTest(){
+        assertThat(car.createRandomNumber()).isGreaterThan(0).isLessThan(10);
+        assertThat(car.createRandomNumber()).isGreaterThan(0).isLessThan(10);
+        assertThat(car.createRandomNumber()).isGreaterThan(0).isLessThan(10);
+    }
+
+}
